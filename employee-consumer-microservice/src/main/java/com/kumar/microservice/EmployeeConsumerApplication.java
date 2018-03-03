77b67b4d6cb9f0b05ffb06a828a1628c038e6682
Saravana Kumar M <kumar9885035454@gmail.com>
2018-03-03 19:46:54 +0530

    Modified few projects to work with Service Discovery and Load Balancer.

--- employee-consumer-microservice/src/main/java/com/kumar/microservice/EmployeeConsumerApplication.java
+++ employee-consumer-microservice/src/main/java/com/kumar/microservice/EmployeeConsumerApplication.java
@@ -2,8 +2,13 @@ package com.kumar.microservice;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -15,8 +20,16 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class EmployeeConsumerApplication
 {
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+
 	public static void main(final String[] args)
 	{
 		SpringApplication.run(EmployeeConsumerApplication.class, args);
@@ -25,8 +38,45 @@ public class EmployeeConsumerApplication
 	@RequestMapping("/employees")
 	public ResponseEntity<String> getEmployees() //throws RestClientException, IOException
 	{
-
 		final String baseUrl = "http://localhost:8001/employees";
+		final RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = null;
+		try
+		{
+			response = restTemplate.exchange(baseUrl, HttpMethod.GET, getHeaders(), String.class);
+		}
+		catch(final Exception ex)
+		{
+			System.out.println(ex);
+		}
+		return response;
+	}
+
+	@RequestMapping("/employees2")
+	public ResponseEntity<String> getEmployeesThroughEurekaServer()
+	{
+		final ServiceInstance serviceInstance = this.discoveryClient.getInstances("employee-producer-microservice").get(0);
+		String baseUrl = serviceInstance.getUri().toString();
+		baseUrl = baseUrl + "/employees";
+		final RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = null;
+		try
+		{
+			response = restTemplate.exchange(baseUrl, HttpMethod.GET, getHeaders(), String.class);
+		}
+		catch(final Exception ex)
+		{
+			System.out.println(ex);
+		}
+		return response;
+	}
+
+	@RequestMapping("/employees3")
+	public ResponseEntity<String> getEmployeesThroughLoadBalancer()
+	{
+		final ServiceInstance serviceInstance = this.loadBalancerClient.choose("employee-producer-microservice");
+		String baseUrl = serviceInstance.getUri().toString();
+		baseUrl = baseUrl + "/employees";
 		final RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = null;
 		try
--- employee-producer-microservice/src/main/java/com/kumar/microservice/EmployeeProducerApplication.java
+++ employee-producer-microservice/src/main/java/com/kumar/microservice/EmployeeProducerApplication.java
@@ -2,8 +2,11 @@ package com.kumar.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class EmployeeProducerApplication
 {
 	public static void main(final String[] args)
