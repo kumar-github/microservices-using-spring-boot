1f481e3ecbdbb964a45aca47d87b2c4e49d31210
Saravana Kumar M <kumar9885035454@gmail.com>
2018-03-11 11:39:51 +0530

    Added a test case
    Added a test case and also tried using the latest @GetMapping, @PostMapping etc annotaions.

--- learning-spring-boot/src/main/java/com/kumar/learningspringboot/restcontroller/HelloController.java
+++ learning-spring-boot/src/main/java/com/kumar/learningspringboot/restcontroller/HelloController.java
@@ -1,23 +1,49 @@
 package com.kumar.learningspringboot.restcontroller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController
 {
-	//@Autowired
-	//private PersonRepository personRepository;
-
-	@org.springframework.web.bind.annotation.RequestMapping
+	@RequestMapping
 	public String sayHelloWorld()
 	{
 		return "You are in new Home Page";
 	}
 
-/*
-	@org.springframework.web.bind.annotation.RequestMapping("/persons/{name}")
-	public Person someMethod(@PathVariable String name)
+	@GetMapping("test")
+	public String handleGetRequest()
 	{
-		return personRepository.findByName(name);
-	}*/
+		return "Get Request Handled";
+	}
+
+	@PostMapping("test")
+	public String handlePostRequest()
+	{
+		return "Post Request Handled";
+	}
+
+	@PutMapping("test")
+	public String handlePutRequest()
+	{
+		return "Put Request Handled";
+	}
+
+	@DeleteMapping("test")
+	public String handleDeleteRequest()
+	{
+		return "Delete Request Handled";
+	}
+
+	@PatchMapping("test")
+	public String handlePatchRequest()
+	{
+		return "Patch Request Handled";
+	}
 }
--- learning-spring-boot/src/test/java/com/kumar/learningspringboot/LearningSpringBootApplicationTests.java
+++ learning-spring-boot/src/test/java/com/kumar/learningspringboot/LearningSpringBootApplicationTests.java
@@ -1,16 +1,49 @@
 package com.kumar.learningspringboot;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LearningSpringBootApplicationTests {
+@WebAppConfiguration
+//@ContextConfiguration(classes = MyWebConfig.class)
+public class LearningSpringBootApplicationTests
+{
+	@Autowired
+	private WebApplicationContext wac;
+
+	private MockMvc mockMvc;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads()
+	{
 	}
 
+	@Before
+	public void setup()
+	{
+		final DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
+		this.mockMvc = builder.build();
+	}
+
+	@Test
+	public void testGet() throws Exception
+	{
+		final ResultMatcher viewMatcher = MockMvcResultMatchers.view().name("Get Request Handled");
+		final MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/test");
+		this.mockMvc.perform(builder).andExpect(viewMatcher).andExpect(MockMvcResultMatchers.status().isOk());
+	}
 }
